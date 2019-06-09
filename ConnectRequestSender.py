81aaa3791af869c6bf3f41f2e20574f05a8fff2f
Sudha <sudha.ramc@gmail.com>
2019-06-09 13:54:23 -0700

    Add geckodriver.log file and add comments in ConnectRequestSender.py program

--- ConnectRequestSender.py
+++ ConnectRequestSender.py
@@ -12,13 +12,13 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 
-
+#Opens Firefox browser to a particular page 
 driver = webdriver.Firefox()
 driver.implicitly_wait(10)
 driver.get("https://www.linkedin.com/login/")
-#driver.get("https://www.linkedin.com/search/results/people/?facetCurrentCompany=%5B%22162479%22%5D&keywords=QA%20manager&origin=GLOBAL_SEARCH_HEADER")
 assert "LinkedIn" in driver.title
 
+#Account Sign in by taking username and password from the user
 username = driver.find_element_by_id("username")
 usrname = input("Please enther the username for this account: ")
 username.clear()
@@ -32,18 +32,23 @@ password.send_keys(pwd)
 signin_button = driver.find_element_by_xpath("/html/body/div/main/div/form/div[3]/button")
 signin_button.click()
 
+#Render the page with specific search terms:-
+#In this program we search for keyword "Apple"(Company) + "QA" + "Manager"
 driver.get("https://www.linkedin.com/search/results/people/?facetCurrentCompany=%5B%22162479%22%5D&keywords=QA%20manager&origin=GLOBAL_SEARCH_HEADER")
 
+#Once the search results are displayed, get the first "Connect" button ID and click 
 connect_button1 = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div[6]/div[3]/div/div[2]/div/div[2]/div/div/div/div/ul/li[1]/div/div/div[3]/div/button')));
 connect_button1.click();
 
+#Connect button click displays a modal to choose between "Add a note" and "Send now"
+#In this pro we sclick the button "Add a note"
 addNote_button = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div[7]/div/div[1]/div/section/div/div[2]/button[1]')));
 addNote_button.click();
 
+#Including a custom message and sending it from the keyboard using Keys class 
 custom_message = driver.find_element_by_id('custom-message')
 message = """Hi,
 I am a Software Engineer with Python scripting and Automation expertise, highly interested in Software QA/ Test Automation Engineer roles. Would like to discuss my background and experience that may align the requirements of your team at Apple. Hoping to astound the world together. 
 Sudha"""
-
 custom_message.clear()
 custom_message.send_keys(message)
